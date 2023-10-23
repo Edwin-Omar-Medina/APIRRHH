058c45f4cf5ae566b22f346802a756879ee3409d
DESKTOP-FT0143J\Admin <edwinmedinac6@gmail.com>
2023-10-22 19:29:29 -0500

    Correcciones del controller

--- RRHH_Datos/Clases/Class1.cs
+++ RRHH_Datos/Clases/Class1.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RRHH_Datos
+namespace RRHH_Datos.Clases
 {
     public class Class1
     {
---
+++ RRHH_Datos/Clases/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRHH_Datos.Clases
+{
+    class Class2
+    {
+    }
+}
